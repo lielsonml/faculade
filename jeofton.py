581adbc6e6bd3dc9fee171d4310112b74fee056b
lielsonml <lielsonml@gmail.com>
2025-04-24 09:50:01 -0300

    print

--- jeofton.py
+++ jeofton.py
@@ -2,7 +2,8 @@ s = int(input(':'))
 match s:
     case 2:
         if s%2== 0:
-            print('o número é par ')
-        
+            print('o número é par')
+        else:
+            print('nao é')
 
 
